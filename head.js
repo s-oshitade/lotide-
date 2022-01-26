ab31b767559d10880eabb5a4538e6bf4518a10fc
Seun Oshitade <seun.oshitade@gmail.com>
2022-01-26 11:16:29 +0000

    Export head, lint head.js

--- head.js
+++ head.js
@@ -5,11 +5,12 @@
 const assertEqual = require('./assertEqual');
 
 const head = (arr) => {
-  return arr[0]
-}
+  return arr[0];
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head([5]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([]), undefined);
 
+module.exports = head;
