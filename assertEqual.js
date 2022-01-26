fd309921e03b4010c5804825b1e0f114fe11ee1c
Seun Oshitade <seun.oshitade@gmail.com>
2022-01-26 10:25:17 +0000

    Export assertEqual function

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,16 @@
-// FUNCTION IMPLEMENTATION
+/**
+ * Function compares two values and logs message accordingly.
+ * @param {*} actual 
+ * @param {*} expected 
+ */
 const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-    : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  actual === expected ? console.log(`\n✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`\n🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-// TEST CODE
 assertEqual("Lighthouse Labs", "Lighthouse Labs");
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual(1, 2);
+
+module.exports = assertEqual;
