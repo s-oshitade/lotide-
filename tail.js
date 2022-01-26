e26dcaf278fb2ec00f24f4fa3f9285524935752c
Seun Oshitade <seun.oshitade@gmail.com>
2022-01-26 12:12:36 +0000

    Add tail function, test cases. eslint!

--- tail.js
+++ tail.js
@@ -1,1 +1,23 @@
+/**
+ * Takes in an array and returns every element except the first.
+ * @param {array} arr
+ * @returns {array}
+ */
 const assertEqual = require('./assertEqual');
+
+const tail = (arr) => {
+  let newArr = [];
+  newArr = arr.slice(1);
+  return newArr;
+};
+
+//Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
