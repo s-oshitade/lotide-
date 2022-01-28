07d1a9314e596f826f2b034b44eb75e8034e3230
Seun Oshitade <seun.oshitade@gmail.com>
2022-01-28 09:42:06 +0000

    Add eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,4 +1,25 @@
 /**
  * takes in 2 arrays and returns true if the arrays are equal and false otherwise.
- * 
+ * @param {Array}
+ * @returns {Boolean}
  */
+const assertEqual = require('./assertEqual');
+
+const eqArrays = (arr1, arr2) => {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    return false;
+  }
+  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  
+  for (let index = 0; index < arr1.length; index++) {
+    if (arr1[index] !== arr2[index]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+module.exports = eqArrays;
