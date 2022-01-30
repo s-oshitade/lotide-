bafe51b1dba584ed3d6a6c031e9f9b0a73c12a41
Seun Oshitade <seun.oshitade@gmail.com>
2022-01-30 19:05:59 +0000

    Add countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -1,1 +1,26 @@
 const assertEqual = require('./assertEqual');
+/**
+ * Function takes in a sentence and counts the letters in the sentence
+ * @param {String} input
+ * @returns {Object}  
+ */
+
+const countLetters = function (input) {
+  inputArray = input.split("")
+  console.log(inputArray)
+  let result = {};
+  for (let element of input){
+    if (element === " "){
+      continue
+    } else {
+      !result[element] ? result[element] = 1 : result[element] += 1;
+    }
+  }
+  return result;
+}
+
+//Test
+
+console.log(countLetters("lighthouse in the house"))
+
+module.exports = countLetters;
