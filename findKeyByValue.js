d3317d76eef5ec8e6a7c6bf49bc41e25460a68f7
Seun Oshitade <seun.oshitade@gmail.com>
2022-01-31 12:12:32 +0000

    Set up findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,13 @@
+const assertEqual = require('./assertEqual');
+
+const findKeyByValue = function (){
+  
+}
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
