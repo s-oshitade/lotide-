f8d1e879075655e8ea22abd94b7c3e0f0b056cea
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-01 02:31:13 +0000

    Refactor test codes

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,5 +1,11 @@
 const eqObjects = require('./eqObjects');
-// Write a function that takes in 2 objects and returns true if they are equal and false otherwise.
+/**
+ * Compares two objects
+ * @param {*} actual
+ * @param {*} expected
+ * @returns boolean
+ * @todo Refactor final function to return boolean
+ */
 
 const assertObjectsEqual = function (actual, expected){
   const inspect = require('util').inspect;
@@ -9,11 +15,11 @@ const assertObjectsEqual = function (actual, expected){
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-assertObjectsEqual(ab, ba);
+assertObjectsEqual(ab, ba); //true
 
-// const cd = { c: "1", d: ["2", 3] };
-// const dc = { d: ["2", 3], c: "1" };
-// assertEqual(eqObjects(cd, dc), true); // => true
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertObjectsEqual(cd, dc); // => true
 
-// const cd2 = { c: "1", d: ["2", 3, 4] };
-// assertEqual(eqObjects(cd, cd2), false); // => false
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertObjectsEqual(cd, cd2); // => false
