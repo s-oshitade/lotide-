689af3c957b3ad202582a0bf2844d8f6a21bc970
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-02 12:47:34 +0000

    Lint takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -1,33 +1,33 @@
 const assertArraysEqual = require('./assertArraysEqual');
 /**
  * Takes in an array and function (callback) and returns a slice of the array based on the criteria specified in the callback.
- * @param {array} 
+ * @param {array}
  * @param {function}
  * @returns {array}
  */
 
-const takeUntil = function(array, callback){
-  let result =[];
-  for (const item of array){
-    if (!callback(item)){
-      result.push(item)
+const takeUntil = function(array, callback) {
+  let result = [];
+  for (const item of array) {
+    if (!callback(item)) {
+      result.push(item);
     } else {
       break;  //Stop looping when callback returns truthy
     }
   }
   return result;
-}
+};
 
 //@example
 const data = ['mercury', 'venus', 'earth', 'moon', 'andromeda', 'milkyway'];
-assertArraysEqual(takeUntil(data, x => x === 'moon'),['mercury', 'venus', 'earth'])
+assertArraysEqual(takeUntil(data, x => x === 'moon'),['mercury', 'venus', 'earth']);
 
-console.log('---'); 
+console.log('---');
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-assertArraysEqual(takeUntil(data1, x => x < 0), [ 1, 2, 5, 7, 2 ])
+assertArraysEqual(takeUntil(data1, x => x < 0), [ 1, 2, 5, 7, 2 ]);
 
-console.log('---'); 
+console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-assertArraysEqual(takeUntil(data2, x => x === ','), ["I've", "been", "to", "Hollywood"])
+assertArraysEqual(takeUntil(data2, x => x === ','), ["I've", "been", "to", "Hollywood"]);
