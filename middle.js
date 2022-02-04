46bbdb554cf50e4fda706e9bc078c0227fdac474
Seun Oshitade <seunoshitade@Seuns-MacBook-Air.local>
2022-02-04 07:42:42 -0700

    Add middle.js

--- middle.js
+++ middle.js
@@ -1,2 +1,25 @@
 const assertArraysEqual = require('./assertArraysEqual');
 const eqArrays = require('./eqArrays');
+/**
+ * Takes in an array and returns the middle most element(s) of the array.
+ * @param {Array} arr
+ * @returns {Array}
+ */
+
+const middle = (arr) => {
+  let middleIndex = Math.ceil(arr.length / 2) - 1; //Minus 1 accounts for zero indexing
+  if (arr.length <= 2) {
+    return [];
+  } else if (arr.length % 2 === 0) {
+    return [arr[middleIndex], arr[middleIndex + 1]];
+  } else {
+    return  [arr[middleIndex]];
+  }
+};
+
+assertArraysEqual((middle([1, 2, 3])), [2]); // => [2]
+assertArraysEqual((middle([1, 2, 3, 4, 5])), [3]); // => [3]
+assertArraysEqual((middle([1, 2, 3, 4])), [2, 3]); // => [2, 3]
+assertArraysEqual((middle([1, 2, 3, 4, 5, 6])), [3, 4]); // => [3, 4]
+
+module.exports = middle;
