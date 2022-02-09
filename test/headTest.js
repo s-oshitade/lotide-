28ee254675fe173d546a80adf7cff6b6b077bbd4
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-09 12:21:42 +0000

    Incorporate Mocha/Chai and include additional tests

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,24 @@
 const head = require('../head');
-const assertEqual = require('../assertEqual');
+const { assert } = require('chai');
 
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns 1001 for [1001, 101, 11]", () => {
+    assert.strictEqual(head([1001, 101, 11]), 1001);
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+  it("returns 'Error: Input is not an array!' for 707", () => {
+    assert.strictEqual(head(707), 'Error: Input is not an array!');
+  });
+});
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head([5]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
