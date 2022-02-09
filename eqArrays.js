9feb5297c8911e23e9cb9e99abd8b080d1b5ac7c
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-09 10:40:28 +0000

    Separate prodn code from test code

--- eqArrays.js
+++ eqArrays.js
@@ -19,12 +19,5 @@ const eqArrays = (arr1, arr2) => {
   return true;
 };
 
-//Test cases
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-
 module.exports = eqArrays;
 
---
+++ test/eqArraysTest.js
@@ -0,0 +1,10 @@
+const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
+
+
+//Test cases
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
