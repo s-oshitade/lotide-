9fc82d0ee26bedb6162db3037f3fcf742d184eb3
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-09 13:33:30 +0000

    Incorporate mocha-chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,29 @@
+const { assert } = require('chai');
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-// Test Cases
-assertArraysEqual((middle([1, 2, 3])), [2]); // => [2]
-assertArraysEqual((middle([1, 2, 3, 4, 5])), [3]); // => [3]
-assertArraysEqual((middle([1, 2, 3, 4])), [2, 3]); // => [2, 3]
-assertArraysEqual((middle([1, 2, 3, 4, 5, 6])), [3, 4]); // => [3, 4]
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("returns ['Lighthouse'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(middle(["Hello", "Lighthouse", "Labs"]), ["Lighthouse"]);
+  });
+
+  it("returns [] for ['tik', 'tok']", () => {
+    assert.deepEqual(middle(['tik', 'tok']), []);
+  });
+
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("returns ['an', 'even'] for ['This', 'is', 'an', 'even', 'number', 'length']", () => {
+    assert.deepEqual(middle(['This', 'is', 'an', 'even', 'number', 'length']), ['an', 'even']);
+  });
+  
+  it("returns 'Error: Input is not an array!' for 202", () => {
+    assert.deepEqual(middle(202), 'Error: Input is not an array!');
+  });
+});
+
