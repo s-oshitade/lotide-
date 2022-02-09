3cc1fa892b18d008f3a1ef37af8b79c84d2b191d
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-09 13:14:14 +0000

    Rafactored code to handle edge case

--- middle.js
+++ middle.js
@@ -1,20 +1,20 @@
-const assertArraysEqual = require('./assertArraysEqual');
-const eqArrays = require('./eqArrays');
 /**
  * Takes in an array and returns the middle most element(s) of the array.
  * @param {Array} arr
  * @returns {Array}
  */
-
 const middle = (arr) => {
-  let middleIndex = Math.ceil(arr.length / 2) - 1; //Minus 1 accounts for zero indexing
-  if (arr.length <= 2) {
-    return [];
-  } else if (arr.length % 2 === 0) {
-    return [arr[middleIndex], arr[middleIndex + 1]];
-  } else {
-    return  [arr[middleIndex]];
+  if (!Array.isArray(arr)) {
+    return "Error: Input is not an array!";
   }
+  let middleIndex = Math.ceil(arr.length / 2) - 1; //Minus 1 accounts for zero indexing
+  if(arr.length <= 2) {
+    return [];
+  } 
+  if(arr.length % 2 === 0) {
+    return [arr[middleIndex], arr[middleIndex + 1]];
+  } 
+  return  [arr[middleIndex]];
 };
 
 module.exports = middle;
