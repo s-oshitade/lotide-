6e819df6c9f5aef77790bab40506a1cbef6450c4
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-09 12:19:54 +0000

    Refactor to handle non-array input

--- head.js
+++ head.js
@@ -1,8 +1,11 @@
 /**
  * Takes in an array and returns the first element in the array.
- * @param {Array} arr
+ * @param {array} arr
  */
 const head = (arr) => {
+  if (!Array.isArray(arr)) {
+    return "Error: Input is not an array!";
+  }
   return arr[0];
 };
 
