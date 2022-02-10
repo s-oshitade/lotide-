e0cd2d3f2292207da0e537998609a54d00471b3a
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-10 11:55:33 +0000

    Remove trailing spaces - eslint

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,7 @@
 /**
  * Takes in 2 values and returns true if they are equal and false otherwise.
- * @param {*} actual 
- * @param {*} expected 
+ * @param {*} actual
+ * @param {*} expected
  */
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
