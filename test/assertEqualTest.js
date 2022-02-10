ff60f0c4b519e740af437d498d7e88dfe53f2fc2
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-10 11:49:51 +0000

    Include tests for edge cases and other data types

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,6 +1,12 @@
-const assertEqual = require('../assertEqual')
+const assertEqual = require('../assertEqual');
 
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2); //
+assertEqual("Lighthouse Labs", "Lighthouse Labs"); //Pass
+assertEqual("Lighthouse Labs", "Bootcamp"); //Fail
+assertEqual(1, 1); //Pass
+assertEqual(1, 2); //Fail
+assertEqual(undefined, undefined); //Pass
+assertEqual(null, null); // Pass
+assertEqual(Number.isNaN(NaN), Number.isNaN(NaN)); //Pass
+assertEqual(Number.isNaN(7), Number.isNaN(7)); //Pass
+assertEqual(Number.isNaN("I love to code!"), Number.isNaN("I love to code!")); //Pass
+assertEqual(NaN, NaN) //Fail - NaN is the only value in JavaScript which is not equal to itself.
