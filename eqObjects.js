bf73c9a8ecbc0ce99eeda0ae85002b445aae31b9
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-11 12:43:38 +0000

    Separate test code from prodn code

--- eqObjects.js
+++ eqObjects.js
@@ -9,11 +9,11 @@ const eqArrays = require('./eqArrays');
 const eqObjects = function(object1, object2) {
   const keysList1 = Object.keys(object1);
   const keysList2 = Object.keys(object2);
-  if (keysList1.length !== keysList2.length) {
+  if (keysList1.length !== keysList2.length) { //check length 
     return false;
   }
   for (let key of keysList1) {
-    if (Array.isArray(object1[key]) || Array.isArray(object2[key])) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       return eqArrays(object1[key], object2[key]);
     }
     if (object1[key] !== object2[key]) {
@@ -22,21 +22,5 @@ const eqObjects = function(object1, object2) {
   }
   return true;
 };
-// @todo Solve for nested object
-
-//Test Cases
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); // => true
-
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false); // => false
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
 
 module.exports = eqObjects;
