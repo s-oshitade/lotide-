d75c291db7188d87e70ca2dbcee588b4dbde95b0
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-11 14:16:47 +0000

    Edit documentation and remove redundant code

--- eqArrays.js
+++ eqArrays.js
@@ -3,14 +3,14 @@
  * @param {Array} arr1
  * @param {Array} arr2
  * @returns {Boolean}
+ * @see {eqArraysRecursive}
  */
-const assertEqual = require('./assertEqual');
 
+//Note: This function is not intended to compare nested arrays or arrays of objects. 
 const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length || !Array.isArray(arr1) || !Array.isArray(arr2)) {
     return false;
   }
-  
   for (let index = 0; index < arr1.length; index++) {
     if (arr1[index] !== arr2[index]) {
       return false;
@@ -20,4 +20,3 @@ const eqArrays = (arr1, arr2) => {
 };
 
 module.exports = eqArrays;
-
