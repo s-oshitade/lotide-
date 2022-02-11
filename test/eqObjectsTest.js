bfee83a9b7532eafb033334be2385fb8a11d68c2
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-11 12:06:17 +0000

    Ad eqObjectsTest.js

---
+++ test/eqObjectsTest.js
@@ -0,0 +1,28 @@
+const assert = require('chai').assert;
+const eqObjects = require('../eqObjects');
+
+describe("#eqObjects", () => {
+  it('returns true for { a: "1", b: "2" }, { a: "1", b: "2" }', () => {
+    assert.deepEqual(eqObjects({ a: "1", b: "2" }, { a: "1", b: "2" }), true);
+  });
+
+  it('returns true for { Name: "Jake", Age: 21 }, { Name: "Jake", Age: 21 }', () => {
+    assert.deepEqual(eqObjects({ Name: "Jake", Age: 21 }, { Name: "Jake", Age: 21 }), true);
+  });
+
+  it('returns true for { Class: "Analgesics", Examples: ["acetaminophen", "ibuprofen", "diclofenac"] }, { Class: "Analgesics", Examples: ["acetaminophen", "ibuprofen", "diclofenac"]}', () => {
+    assert.deepEqual(eqObjects({ Class: "Analgesics", Examples: ["acetaminophen", "ibuprofen", "diclofenac"] }, { Class: "Analgesics", Examples: ["acetaminophen", "ibuprofen", "diclofenac"]  }), true);
+  });
+
+  it('returns false for { a: "2", b: "2" }, { a: "1", b: "2" }', () => {
+    assert.deepEqual(eqObjects({ a: "2", b: "2" }, { a: "1", b: "2" }), false);
+  });
+
+  it('returns false for { Name: "Drake", Age: 21 }, { Name: "Jake", Age: 21 }', () => {
+    assert.deepEqual(eqObjects({ Name: "Drake", Age: 21 }, { Name: "Jake", Age: 21 }), false);
+  });
+
+  it('returns false for { Class: "Analgesics", Examples: ["acetaminophen", "ibuprofen"] }, { Class: "Analgesics", Examples: ["acetaminophen", "ibuprofen", "diclofenac"]}', () => {
+    assert.deepEqual(eqObjects({ Class: "Analgesics", Examples: ["acetaminophen", "ibuprofen"] }, { Class: "Analgesics", Examples: ["acetaminophen", "ibuprofen", "diclofenac"]  }), false);
+  });
+});
