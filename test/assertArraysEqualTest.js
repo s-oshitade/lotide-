0e942e49cd628ab1ab4e39cbdf8b8ca0a51d5e13
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-15 13:53:23 +0000

    Add mocha and chai tests for assertArraysEqual

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,8 +1,20 @@
 const assertArraysEqual = require('../assertArraysEqual');
-const eqArrays = require('../eqArrays');
+const {assert} = require('chai');
 
-// Test Cases
-assertArraysEqual([1, 2, 3], [1, 2, 3])
-assertArraysEqual([1, 3, 3], [1, 2, 3])
-
-
+describe("#assertArraysEqual", () => {
+  it("returns [1, 2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+  });
+  it("returns [1, 3] for [1, 3]", () => {
+    assert.deepEqual(assertArraysEqual([1, 3], [1, 3]));
+  });  
+  it("returns [17, 21, 33] for [17, 21, 33]", () => {
+    assert.deepEqual(assertArraysEqual([17, 21, 33], [17, 21, 33]));
+  });  
+  it("returns ['Seun', 'Sarah'] for ['Seun', 'Sarah']", () => {
+    assert.deepEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+  });  
+  it("returns ['Seun', 'Sarah'] for ['Seun', 'Sarah']", () => {
+    assert.deepEqual(assertArraysEqual(['Seun', 'Sarah'], ['Seun', 'Sarah']));
+  });  
+});
