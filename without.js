9320d84741e8fac40a2723208973d76cbcc181f9
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-15 14:01:17 +0000

    Separate prodn code from test code

--- without.js
+++ without.js
@@ -1,12 +1,9 @@
-const assertArraysEqual = require('./assertArraysEqual');
-const eqArrays = require('./eqArrays');
 /**
  * Function removes unwanted elements from a given array
  * @param {*} source
  * @param {*} itemsToRemove
  * @returns array
  */
-
 const without = (source, itemsToRemove) => {
   let result = [];
   for (const item of source) {
@@ -16,9 +13,5 @@ const without = (source, itemsToRemove) => {
   }
   return result;
 };
-
-//Test cases
-// assertArraysEqual(without([1, 2, 3], [1]), [2, 3]) // => [2, 3]
-// assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"] ); // => ["1", "2"]
 
 module.exports = without;
