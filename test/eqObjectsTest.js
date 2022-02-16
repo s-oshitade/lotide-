a8ac5974dd4e4df9f8519a32a02fb85e0da103f2
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-16 13:31:57 +0000

    eqObjectsTest.js

--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -10,20 +10,20 @@ describe("#eqObjects", () => {
     assert.deepEqual(eqObjects({ c: "1", d: ["2", 3] }, { d: ["2", 3], c: "1" }), true);
   });
 
-  it('returns true for { Name: "Jake", Age: 21 }, { Name: "Jake", Age: 21 }', () => {
-    assert.deepEqual(eqObjects({ Name: "Jake", Age: 21 }, { Name: "Jake", Age: 21 }), true);
-  });
-
-  it('returns true for { Class: "Analgesics", Examples: ["acetaminophen", "ibuprofen", "diclofenac"] }, { Class: "Analgesics", Examples: ["acetaminophen", "ibuprofen", "diclofenac"]}', () => {
-    assert.deepEqual(eqObjects({ Class: "Analgesics", Examples: ["acetaminophen", "ibuprofen", "diclofenac"] }, { Class: "Analgesics", Examples: ["acetaminophen", "ibuprofen", "diclofenac"]  }), true);
-  });
-
   it('returns false for { a: "2", b: "2" }, { a: "1", b: "2" }', () => {
     assert.deepEqual(eqObjects({ a: "2", b: "2" }, { a: "1", b: "2" }), false);
   });
 
+  it('returns true for { Name: "Jake", Age: 21 }, { Name: "Jake", Age: 21 }', () => {
+    assert.deepEqual(eqObjects({ Name: "Jake", Age: 21 }, { Name: "Jake", Age: 21 }), true);
+  });
+
   it('returns false for { Name: "Drake", Age: 21 }, { Name: "Jake", Age: 21 }', () => {
     assert.deepEqual(eqObjects({ Name: "Drake", Age: 21 }, { Name: "Jake", Age: 21 }), false);
+  });
+
+  it('returns true for { Class: "Analgesics", Examples: ["acetaminophen", "ibuprofen", "diclofenac"] }, { Class: "Analgesics", Examples: ["acetaminophen", "ibuprofen", "diclofenac"]}', () => {
+    assert.deepEqual(eqObjects({ Class: "Analgesics", Examples: ["acetaminophen", "ibuprofen", "diclofenac"] }, { Class: "Analgesics", Examples: ["acetaminophen", "ibuprofen", "diclofenac"]  }), true);
   });
 
   it('returns false for { Class: "Analgesics", Examples: ["acetaminophen", "ibuprofen"] }, { Class: "Analgesics", Examples: ["acetaminophen", "ibuprofen", "diclofenac"]}', () => {
