d993d7bba993ceac41a9adccf3b7e140852e8355
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-16 18:05:00 +0000

    Create eqObjectsRecursive fxn, starting with codes from eqObjects.js

---
+++ eqObjectsRecursive.js
@@ -0,0 +1,25 @@
+const eqArrays = require('./eqArrays');
+/**
+ * Takes in 2 objects and returns true if they are equal and false otherwise. Supports nested objects.
+ * @param {*} object1
+ * @param {*} object2
+ * @returns boolean
+ */
+const eqObjectsRecursive = function(object1, object2) {
+  const keysList1 = Object.keys(object1);
+  const keysList2 = Object.keys(object2);
+  if (keysList1.length !== keysList2.length) { //check length
+    return false;
+  }
+  for (let key of keysList1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      return eqArrays(object1[key], object2[key]);
+    }
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+module.exports = eqObjectsRecursive;
