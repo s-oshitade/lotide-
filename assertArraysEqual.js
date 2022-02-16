bbe62178b3b805d811f3f606de00465a35ab5d03
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-16 11:44:22 +0000

    Refactor fxn to only return boolean values

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,14 +3,13 @@ const eqArrays = require('./eqArrays');
  * Takes in 2 arrays and returns true if they are equal and false otherwise.
  * @param {*} actual
  * @param {*} expected
+ * @return {boolean}
  */
 
 const assertArraysEqual = (actual, expected) => {
   if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed!: ${actual} === ${expected}`);//Allow assert function to print to console.
     return true;
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed!: ${actual} !== ${expected}`);
     return false;
   }
 };
