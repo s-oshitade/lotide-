6dc98a5530b875633916830d2f5e805fe8323ae8
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-16 11:59:37 +0000

    Correct the chai assertion statements in test suite

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -2,19 +2,27 @@ const assertArraysEqual = require('../assertArraysEqual');
 const {assert} = require('chai');
 
 describe("#assertArraysEqual", () => {
-  it("returns [1, 2, 3] for [1, 2, 3]", () => {
-    assert.deepEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+  it("returns true for [1, 2, 3], 1, 2, 3]", () => {
+    assert.deepEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]), true);
   });
-  it("returns [1, 3] for [1, 3]", () => {
-    assert.deepEqual(assertArraysEqual([1, 3], [1, 3]));
-  });  
-  it("returns [17, 21, 33] for [17, 21, 33]", () => {
-    assert.deepEqual(assertArraysEqual([17, 21, 33], [17, 21, 33]));
-  });  
-  it("returns ['Seun', 'Sarah'] for ['Seun', 'Sarah']", () => {
-    assert.deepEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]));
-  });  
-  it("returns ['Seun', 'Sarah'] for ['Seun', 'Sarah']", () => {
-    assert.deepEqual(assertArraysEqual(['Seun', 'Sarah'], ['Seun', 'Sarah']));
-  });  
+
+  it("returns true for [1, 3], [1, 3]", () => {
+    assert.deepEqual(assertArraysEqual([1, 3], [1, 3]), true);
+  });
+
+  it("returns true for [17, 21, 33], [17, 21, 33]", () => {
+    assert.deepEqual(assertArraysEqual([17, 21, 33], [17, 21, 33]), true);
+  });
+
+  it("returns false for ['Seun', 'Sarah'], ['Martha', 'Sarah']", () => {
+    assert.deepEqual(assertArraysEqual(['Seun', 'Sarah'], ['Martha', 'Sarah']), false);
+  });
+
+  it("returns false for ['Seun', 'Sarah', 'Martha'], ['Seun', 'Sarah']", () => {
+    assert.deepEqual(assertArraysEqual(['Seun', 'Sarah', 'Martha'], ['Seun', 'Sarah']), false);
+  });
+  
+  it("returns false for ['James'], ['Jack']", () => {
+    assert.deepEqual(assertArraysEqual(['James'], ['Jack']), false);
+  });
 });
