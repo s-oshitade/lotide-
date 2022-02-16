561acf457d7c84ba020086dbe0e09bd834a7bc08
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-16 13:24:38 +0000

    Test fxn with mocha and chai

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,12 +1,32 @@
 const assertEqual = require('../assertEqual');
+const {assert} = require('chai');
 
-assertEqual("Lighthouse Labs", "Lighthouse Labs"); //Pass
-assertEqual("Lighthouse Labs", "Bootcamp"); //Fail
-assertEqual(1, 1); //Pass
-assertEqual(1, 2); //Fail
-assertEqual(undefined, undefined); //Pass
-assertEqual(null, null); // Pass
-assertEqual(Number.isNaN(NaN), Number.isNaN(NaN)); //Pass
-assertEqual(Number.isNaN(7), Number.isNaN(7)); //Pass
-assertEqual(Number.isNaN("I love to code!"), Number.isNaN("I love to code!")); //Pass
-assertEqual(NaN, NaN); //Fail - NaN is the only value in JavaScript which is not equal to itself.
+describe("#assertEqual", () => {
+  it('returns true for "Lighthouse Labs", "Lighthouse Labs"', () => {
+    assert.strictEqual(assertEqual("Lighthouse Labs", "Lighthouse Labs"), true);
+  });
+  it('returns true for 1, 1', () => {
+    assert.strictEqual(assertEqual(1, 1), true);
+  });
+  it('returns false for 1, 2', () => {
+    assert.strictEqual(assertEqual(1, 2), false);
+  });
+  it('returns true for undefined, undefined', () => {
+    assert.strictEqual(assertEqual(undefined, undefined), true);
+  });
+  it('returns true for null, null', () => {
+    assert.strictEqual(assertEqual(null, null), true);
+  });
+  it('returns true for Number.isNaN(NaN), Number.isNaN(NaN)', () => {
+    assert.strictEqual(assertEqual(Number.isNaN(NaN), Number.isNaN(NaN)), true);
+  });
+  it('returns true for Number.isNaN(7), Number.isNaN(7)', () => {
+    assert.strictEqual(assertEqual(Number.isNaN(7), Number.isNaN(7)), true);
+  });
+  it('returns true for Number.isNaN("I love to code!"), Number.isNaN("I love to code!")', () => {
+    assert.strictEqual(assertEqual(Number.isNaN(7), Number.isNaN(7)), true);
+  });
+  it('returns true for NaN, NaN', () => {
+    assert.strictEqual(assertEqual(NaN, NaN), false); //NaN is the only value in JavaScript which is not equal to itself.
+  });
+});
