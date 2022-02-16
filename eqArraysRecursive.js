3a7037b38d16f5561daf20627d2bebf9a216a3d3
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-16 17:13:28 +0000

    Add recursive eqArrays fxn

--- eqArraysRecursive.js
+++ eqArraysRecursive.js
@@ -4,42 +4,40 @@
  * @param {Array} arr2
  * @returns {Boolean}
  */
- const assertEqual = require('./assertEqual');
+function eqArraysRecursive (arr1, arr2) {
+  if (arr1.length !== arr2.length || !Array.isArray(arr1) || !Array.isArray(arr2)) {
+    return false;
+  }
+ 
+  for (let index = 0; index < arr1.length; index++) {
+    if (!Array.isArray(arr1[index]) || !Array.isArray(arr2[index])) {
+      if (arr1[index] !== arr2[index]) {
+        return false;
+      }
+    }
+    if (Array.isArray(arr1[index]) && Array.isArray(arr2[index])) {
+      if (!eqArraysRecursive(arr1[index], arr2[index])) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
 
- const eqArraysRecursive = function(arr1, arr2) {
-   if (arr1.length !== arr2.length || !Array.isArray(arr1) || !Array.isArray(arr2)) {
-     return false;
-   }
- 
-   for (let index = 0; index < arr1.length; index++) {
-     if (!Array.isArray(arr1[index]) || !Array.isArray(arr2[index])) {
-       if (arr1[index] !== arr2[index]) {
-         return false;
-       }
-     }
-     if (Array.isArray(arr1[index]) && Array.isArray(arr2[index])) {
-         if(!eqArraysRecursive(arr1[index], arr2[index])){
-           return false;
-         }
-     }
-   }
-   return true;
- };
- 
- //  Test cases
- assertEqual(eqArraysRecursive([1, 2, 3], [1, 2, 3]), true); // => true
- assertEqual(eqArraysRecursive([1, 2, 3], [3, 2, 1]), false); // => false
+module.exports = eqArraysRecursive;
+//  Test cases
+//  assertEqual(eqArraysRecursive([1, 2, 3], [1, 2, 3]), true); // => true
+//  assertEqual(eqArraysRecursive([1, 2, 3], [3, 2, 1]), false); // => false
   
- assertEqual(eqArraysRecursive(["1", "2", "3"], ["1", "2", "3"]), true); // => true
- assertEqual(eqArraysRecursive(["1", "2", "3"], ["1", "2", 3]), false); // => false
+//  assertEqual(eqArraysRecursive(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+//  assertEqual(eqArraysRecursive(["1", "2", "3"], ["1", "2", 3]), false); // => false
  
- console.log("____Nested arrays tested below____");
+//  console.log("____Nested arrays tested below____");
  
- assertEqual(eqArraysRecursive([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+//  assertEqual(eqArraysRecursive([[2, 3], [4]], [[2, 3], [4]]), true); // => true
  
- assertEqual(eqArraysRecursive([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
- assertEqual(eqArraysRecursive([[2, 3], [4]], [[2, 3], 4]), false) // => false
+//  assertEqual(eqArraysRecursive([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
+//  assertEqual(eqArraysRecursive([[2, 3], [4]], [[2, 3], 4]), false) // => false
   
- module.exports = eqArraysRecursive;
   
   
