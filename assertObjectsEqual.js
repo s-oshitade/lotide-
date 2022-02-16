3bd25cf4f94cba3a71700757ee2f79c71971ac08
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-16 12:23:28 +0000

    Separate prodn code from test code

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -14,16 +14,4 @@ const assertObjectsEqual = function (obj1, obj2){
   }
 };
 
-
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-console.log(assertObjectsEqual(ab, ba)); //true
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-console.log(assertObjectsEqual(cd, dc)); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(assertObjectsEqual(cd, cd2)); // => false
-
 module.exports = assertObjectsEqual;
