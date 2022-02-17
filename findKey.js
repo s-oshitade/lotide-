84d510c8516b64a826e1dc3079a5a966228a635a
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-17 07:36:03 +0000

    Separate prodn code from test code

--- findKey.js
+++ findKey.js
@@ -1,7 +1,6 @@
-const assertEqual = require('./assertEqual');
 /**
- * Takes in an object and callback and returns the first key that meets the criteria specified in callback.
- * @param {object}
+ * Takes in an object and callback and returns the first key for which the callback returns a truthy value
+ * @param {object} myObj
  * @param {function} callback
  * @returns {*}
  */
@@ -15,25 +14,6 @@ const findKey = (myObj, callback) => {
   }
 };
 
-// Test cases
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, val => val.stars === 2), "noma");
-
-console.log('---');
-
-const obj = {
-  "the expanse" : "sci_fi",
-  "the big bang theory!" : "comedy",
-  "the wire" : "drama"
-};
-assertEqual(findKey(obj, val => val === "comedy"), "the big bang theory!");
-
-
+module.exports = findKey;
 
 
