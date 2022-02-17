6fb99b8afd80365818fb9257ecd1f27cf09e6566
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-17 07:01:56 +0000

    Separate prodn code from test code

--- map.js
+++ map.js
@@ -14,13 +14,4 @@ const map = (array, callback) => {
   return results;
 }
 
-//Test Cases
-assertArraysEqual(map(['Jake', 'Hannah', 'Martha'], fName => `${fName} Oshitade`), ['Jake Oshitade', 'Hannah Oshitade', 'Martha Oshitade'], true);
-
-const cities = ['Calgary', 'Edmonton', 'Toronto'];
-assertArraysEqual(map(cities, city => `${city[0]}${city[1]}${city[2]}`), ['Cal', 'Edm', 'Tor']);
-
-const grades = ['Maths A', 'Physics B+', 'Programming A+'];
-assertArraysEqual(map(grades, grade => `${grade.slice(- 2).trim()}`), ['A', 'B+', 'A+']);
-
 module.exports = map;
