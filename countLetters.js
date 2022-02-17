ac4887412155c9e3721a49d156927a2fd893c97c
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-17 00:33:26 +0000

    Separate prodn code from test code

--- countLetters.js
+++ countLetters.js
@@ -1,26 +1,20 @@
-const assertEqual = require('./assertEqual');
 /**
- * Function takes in a sentence and counts the letters in the sentence
- * @param {String} input
- * @returns {Object}  
+ * Function takes in a sentence and returns a count of EACH of the letters in the sentence.
+ * @param {string}
+ * @returns {object}
  */
 
-const countLetters = function (input) {
+const countLetters = function(input) {
   input = input.toLocaleLowerCase();
-  inputArray = input.split("")
   let result = {};
-  for (let element of input){
-    if (element === " "){
-      continue
+  for (let element of input) {
+    if (element === " ") {
+      continue;
     } else {
       !result[element] ? result[element] = 1 : result[element] += 1;
     }
   }
   return result;
-}
-
-//Test
-
-console.log(countLetters("lighthouse in the house"))
+};
 
 module.exports = countLetters;
