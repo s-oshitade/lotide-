69c84032468228427088cc57ed4c84d8ae23fb50
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-17 07:47:36 +0000

    Separate prodn code from test code

--- takeUntil.js
+++ takeUntil.js
@@ -18,16 +18,4 @@ const takeUntil = function(array, callback) {
   return result;
 };
 
-//@example
-const data = ['mercury', 'venus', 'earth', 'moon', 'andromeda', 'milkyway'];
-assertArraysEqual(takeUntil(data, x => x === 'moon'),['mercury', 'venus', 'earth']);
-
-console.log('---');
-
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-assertArraysEqual(takeUntil(data1, x => x < 0), [ 1, 2, 5, 7, 2 ]);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-assertArraysEqual(takeUntil(data2, x => x === ','), ["I've", "been", "to", "Hollywood"]);
+module.exports = takeUntil;
