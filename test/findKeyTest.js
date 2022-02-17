c51d2ca5a73059aec1f60df017ccaea0e609cf72
Seun Oshitade <seun.oshitade@gmail.com>
2022-02-17 08:11:51 +0000

    Add test suite incorporating mocha and chai

--- test/findKeyTest.js
+++ test/findKeyTest.js
@@ -1,7 +1,6 @@
 const {assert} = require('chai');
 const findKey = require('../findKey');
 
-
 describe("#findKeys", () => {
   it(`returns "noma" for {"Akaleri": { stars: 3 },"noma": { stars: 2 }, "elBulli": { stars: 2 }}, val => val.stars === 2`, () => {
     assert.deepEqual(findKey({"Akaleri": { stars: 3 },"noma": { stars: 2 }, "elBulli": { stars: 2 }}, val => val.stars === 2), "noma");
